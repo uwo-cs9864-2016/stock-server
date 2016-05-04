9d7ecab892975506b5ad392de822fbaa02643f03
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-05-04 15:14:14 -0400

    Host docs

---
+++ docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "title": "CS9864 Stock Streaming Server",  "name": "stock-server",  "version": "0.0.1",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-05-04T19:11:53.576Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
